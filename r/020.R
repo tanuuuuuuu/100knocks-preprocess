d7d273df27b1300553d5c05927f49d659bb28510
tanuuuuuuu <tanu@boul.tech>
2025-08-17 23:57:28 +0900

    add R 11-20

---
+++ r/020.R
@@ -0,0 +1,27 @@
+# 順位 / 順位付けする（同一順位なし）（level2）
+# レシート明細データに対し、1件あたりの売上金額（amount）が高い順にランクを付与し、先頭から10件表示せよ。
+# 項目は顧客ID（customer_id）、売上金額（amount）、付与したランクを表示させること。
+# なお、売上金額が等しい場合でも別順位を付与すること。
+
+library(tidyverse)
+
+df_receipt <- read_csv("./data/receipt.csv")
+df_receipt |>
+    select(customer_id, amount) |> 
+    mutate(rank = row_number(desc(amount))) |> 
+    arrange(rank) |> 
+    slice_head(n = 10)
+
+# A tibble: 10 × 3
+#    customer_id    amount  rank
+#    <chr>           <dbl> <int>
+#  1 CS011415000006  10925     1
+#  2 ZZ000000000000   6800     2
+#  3 CS028605000002   5780     3
+#  4 CS015515000034   5480     4
+#  5 ZZ000000000000   5480     5
+#  6 ZZ000000000000   5480     6
+#  7 ZZ000000000000   5440     7
+#  8 CS021515000089   5440     8
+#  9 CS015515000083   5280     9
+# 10 CS017414000114   5280    10
